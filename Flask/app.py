a3d727e018fa306789a8e5a1745e8a96dc2133d8
Anna Burlyaeva <46877591+annaburlyaeva@users.noreply.github.com>
2019-06-24 16:30:13 -0700

    Added presentation

--- Flask/app.py
+++ Flask/app.py
@@ -1,14 +1,54 @@
 # import necessary libraries
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, request
 # from user_data import *
 import data_prep
+from flask import request
+from flask_mysqldb import MySQL
+from config import password
+from flask_sqlalchemy import SQLAlchemy
+
 
 # create instance of Flask app
 app = Flask(__name__)
 
-CORS(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{password}@127.0.0.1:3306/health_monitor_db'
 
+db = SQLAlchemy(app)
+
+# from models import Users_indicators_values
+# from models import Indicators
+class Users_indicators_values(db.Model):
+
+    __tablename__ = 'users_indicators_values'
+
+    record_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    indicator_id = db.Column(db.Integer,db.ForeignKey('indicators.indicator_id'))
+    record_date = db.Column(db.DateTime)
+    indicator_value = db.Column(db.Float)
+    notes = db.Column(db.String(64))
+
+#     def __init__(self, user_id , indicator_id, record_date, indicator_value,  note):
+#         self.user_id = user_id
+#         self.indicator_id = indicator_id
+#         self.record_date = record_date
+#         self.indicator_value = indicator_value
+#         self.note = note
+
+    def __repr__(self):
+        return '<Users_indicators_values %r>' % (self.indicator_value)
+
+class Indicators(db.Model):
+
+    __tablename__ = 'indicators'
+
+    indicator_id = db.Column(db.Integer, primary_key=True)
+    indicator_name = db.Column(db.String(200))
+    indicator_type = db.Column(db.String(20))
+
+    def __repr__(self):
+        return '<Indicators %r>' % (self.indicator_name)
 
 
 # create route that renders index.html template
@@ -18,7 +58,9 @@ def index():
         f"Available Routes:<br/>"
         f"/user_json"
         f"<br/>"
-        f"/indicators")
+        f"/indicators"
+        f"<br/>"
+        f"/add_data")
 
 
 @app.route('/user_json')
@@ -30,9 +72,6 @@ def user_json():
 @app.route("/indicators")
 def indicators():
        data = data_prep.data_prep('health_monitor_db', 'juliette_leblanc')
-       indicators = data["indicators"]
-       list_indic = [i['indicator_name'] for i in indicators]
-       
        indicators_data = data["indicators"]
        indicators_list = [list_item["indicator_name"] for list_item in indicators_data]
        indicators_dict = {}
@@ -45,6 +84,23 @@ def indicators():
        indicators_dict["indicators"] = nested_indicators_list
 
        return jsonify(indicators_dict)
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+
+       request_data = request.get_json()
+       user_id = 1
+       indicator_id = db.session.query(Indicators.indicator_id).filter(Indicators.indicator_name == request_data['indicator_name'])[0]
+       record_date = request_data['record_date']
+       indicator_value = request_data['indicator_value']
+       notes = request_data['notes']
+
+       users_indicators_values = Users_indicators_values(user_id=user_id, indicator_id=indicator_id, record_date=record_date, indicator_value=indicator_value, notes=notes)
+       db.session.add(users_indicators_values)
+       db.session.commit()
+
+       return "Done", 201
+
 
 
 
